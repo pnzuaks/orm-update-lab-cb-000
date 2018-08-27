e1003cbc43ad0690efbe2862dac296e48b8a01b6
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 20:40:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
     student.save()
     student
   end
-  
+
   def self.new_from_db(row)
      # create a new Student object given a row from the database
      student = self.new
@@ -52,4 +52,20 @@ class Student
      student.grade = row[2]
      student
    end
+
+   def self.find_by_name(name)
+   # find the student in the database given a name
+   # return a new instance of the Student class
+   sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE name = ?
+     LIMIT 1
+   SQL
+
+   DB[:conn].execute(sql,name).map do |row|
+     self.new_from_db(row)
+   end.first
+ end
+ 
 end
