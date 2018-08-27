cab4924aff68db4082afe88fbe2f2f6dacd4a8d8
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 20:51:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,6 @@ class Student
    end
 
    def self.find_by_name(name)
-   # find the student in the database given a name
-   # return a new instance of the Student class
    sql = <<-SQL
      SELECT *
      FROM students
@@ -67,7 +65,7 @@ class Student
      LIMIT 1
    SQL
 
-   DB[:conn].execute(sql,name)
+   puts DB[:conn].execute(sql,name)
 
   #  map do |row|
   #    self.new_from_db(row)
