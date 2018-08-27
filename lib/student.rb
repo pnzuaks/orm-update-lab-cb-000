151930dce043b9157c3fa23c4ae39cc599fa8b26
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 20:59:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,9 +52,6 @@ class Student
   def self.new_from_db(row)
      # create a new Student object given a row from the database
      student = self.new(row[1], row[2], row[0])
-     student.id = row[0]
-     student.name = row[1]
-     student.grade = row[2]
      student
    end
 
