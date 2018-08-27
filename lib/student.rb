95c4f8c87d29f4ba098b0e1ddbbf1a4259db491f
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-27 20:38:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,5 +9,33 @@ class Student
     @grade = grade
   end
 
+  def self.create_table()
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+        SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table()
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+        SQL
+      DB[:conn].execute(sql)
+  end
+
+  def save()
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 
 end
